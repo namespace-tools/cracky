bb36e15504ed852273989a91f361301215765e05
namespace-ctfetc <76843450+namespace-ctfetc@users.noreply.github.com>
2021-11-03 21:15:04 +0000

    Add files via upload

---
+++ cracky.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Cracky-Py: Because sometimes nothing else works.
+#
+# USAGE: cracky.py [HashType] [HashFile] [WordList]
+
+from hashlib import *
+from sys import argv
+
+HashType = argv[1]
+
+print(
+"""\x1b[36m————————————————————————————————————————————————————————————————
+\x1b[31m_________                       __            __________        
+\_   ___ \____________    ____ |  | _____.__. \______   \___.__.
+/    \  \/\_  __ \__  \ _/ ___\|  |/ <   |  |  |     ___<   |  |
+\     \____|  | \// __ \\  \___|     < \___  |  |    |    \___  |
+ \______  /|__|  (____  /\___  >__|_ \/ ____|  |____|    / ____|
+        \/            \/     \/     \/\/                 \/     
+\x1b[36m————————————————————————————————————————————————————————————————\033[0m""")
+
+with open(argv[2]) as infile:
+	hash = str.strip(infile.readline())
+
+with open(argv[3]) as wordlist:
+	for Line in wordlist:
+		x = str.strip(Line)
+		if globals()[HashType](bytes(''.join(x),'utf8')).hexdigest() == hash:
+				print(f">> Password: {''.join(x)}\n")
+				exit()
+
+print(f" Unable to crack: {hash}")
