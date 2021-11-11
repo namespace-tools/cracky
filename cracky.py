7dc01f200443f1372fb6bdff9c568591eb395c43
namespace-ctfetc <76843450+namespace-ctfetc@users.noreply.github.com>
2021-11-11 17:21:53 -0500

    Add files via upload
    Minor tweaks for speed.
    
    Still to come... Support for additional hash types

--- cracky.py
+++ cracky.py
@@ -11,22 +11,21 @@ HashType = argv[1]
 
 print(
 """\x1b[36m————————————————————————————————————————————————————————————————
-\x1b[31m_________                       __            __________        
+\x1b[31m_________                       __            __________
 \_   ___ \____________    ____ |  | _____.__. \______   \___.__.
 /    \  \/\_  __ \__  \ _/ ___\|  |/ <   |  |  |     ___<   |  |
 \     \____|  | \// __ \\  \___|     < \___  |  |    |    \___  |
  \______  /|__|  (____  /\___  >__|_ \/ ____|  |____|    / ____|
-        \/            \/     \/     \/\/                 \/     
+        \/            \/     \/     \/\/                 \/
 \x1b[36m————————————————————————————————————————————————————————————————\033[0m""")
 
-with open(argv[2], encoding='latin-1') as infile:
+with open(argv[2]) as infile:
 	hash = str.strip(infile.readline())
 
-with open(argv[3]) as wordlist:
+with open(argv[3], encoding='latin-1') as wordlist:
 	for Line in wordlist:
-		x = str.strip(Line)
-		if globals()[HashType](bytes(''.join(x),'latin-1')).hexdigest() == hash:
-				print(f">> Password: {''.join(x)}\n")
+		if globals()[HashType](bytes(Line.strip(), 'latin-1')).hexdigest() == hash:
+				print(f">> Password: {Line}\n")
 				exit()
 
 print(f" Unable to crack: {hash}")
